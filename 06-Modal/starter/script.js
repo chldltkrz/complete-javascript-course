cd7f6630505e5b3b080be73a898f33533cc27b1a
DESKTOP-VFH9NV2\test <choiis@goodmit.co.kr>
2021-07-14 16:53:36 +0900

    Add modal functionality

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+// this querySelectorAll will select all elements matching in the name
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const removeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//add eventlistener to buttons to display modal window
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+//add eventlistener to 'x' button and overlay to hide modal window
+btnCloseModal.addEventListener('click', removeModal);
+overlay.addEventListener('click', removeModal);
+
+//add global eventlistener to 'Esc' key to hide modal window
+document.addEventListener('keydown', function (E) {
+  if (E.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      removeModal();
+    }
+  }
+});
